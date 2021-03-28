1172215d0fe0717ea4426086a5feabb5a7d71d87
Thin Tran <trvathin@gmail.com>
2021-03-28 16:01:26 +0700

    Init screens

---
+++ src/screens/LibraryScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function LibraryScreen(props) {
+  return (
+    <>
+      <View>
+        <Text>LibraryScreen</Text>
+      </View>
+    </>
+  );
+}
+
+export default LibraryScreen;
---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function SettingsScreen(props) {
+  return (
+    <>
+      <View>
+        <Text>SettingsScreen</Text>
+      </View>
+    </>
+  );
+}
+
+export default SettingsScreen;
