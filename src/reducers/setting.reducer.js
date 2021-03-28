eb8ee3747d82ede21ce9b98f76912c721b54c7e9
Thin Tran <trvathin@gmail.com>
2021-03-28 19:51:23 +0700

    Refactor action

--- src/reducers/setting.reducer.js
+++ src/reducers/setting.reducer.js
@@ -1,10 +1,12 @@
+import {SET_THEME} from '../actions/setting.actions';
+
 const INITIAL_STATE = {
   theme: 'light',
 };
 
 export default function (state = INITIAL_STATE, {type, payload}) {
   switch (type) {
-    case 'SET_THEME':
+    case SET_THEME:
       return {...state, theme: payload};
     default:
       return state;
