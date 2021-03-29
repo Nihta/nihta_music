01ec78ce5e5c664d8f1b581a1e6e272698b94790
Thin Tran <trvathin@gmail.com>
2021-03-29 22:41:19 +0700

    Init LibraryNavigator

---
+++ src/navigations/LibraryNavigator.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+
+// Components
+import Icon from '../components/Icon';
+
+// Screens
+import AlbumsScreen from '../screens/AlbumsScreen';
+import ArtistsScreen from '../screens/ArtistsScreen';
+import FoldersScreen from '../screens/FoldersScreen';
+import LibraryScreen from '../screens/LibraryScreen';
+import PlaylistsScreen from '../screens/PlaylistsScreen';
+
+const screenOptions = {
+  ...TransitionPresets.ScaleFromCenterAndroid,
+  headerStyle: {
+    elevation: 0,
+  },
+  headerTitleStyle: {
+    fontFamily: 'Circular',
+    fontWeight: '400',
+    fontSize: 18,
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  headerTitleAlign: 'center',
+  headerBackImage: () => <Icon type="feather" name="chevron-left" size={26} />,
+};
+
+const Stack = createStackNavigator();
+
+function LibraryNavigator() {
+  return (
+    <Stack.Navigator headerMode="screen" screenOptions={screenOptions}>
+      <Stack.Screen
+        name="library"
+        component={LibraryScreen}
+        options={{title: 'Thư Viện Của Tôi'}}
+      />
+      <Stack.Screen
+        name="playlists"
+        component={PlaylistsScreen}
+        options={{title: 'Danh sách phát'}}
+      />
+      <Stack.Screen
+        name="artists"
+        component={ArtistsScreen}
+        options={{title: 'Nghệ Sĩ'}}
+      />
+      <Stack.Screen
+        name="albums"
+        component={AlbumsScreen}
+        options={{title: 'Albums'}}
+      />
+      <Stack.Screen
+        name="folders"
+        component={FoldersScreen}
+        options={{title: 'Thư mục'}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default LibraryNavigator;
