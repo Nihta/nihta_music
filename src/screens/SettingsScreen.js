84cc5b06bc6d662247b003c8cc2d0d56f30ec3ad
Thin Tran <trvathin@gmail.com>
2021-03-29 22:39:46 +0700

    Update setting screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,14 +1,92 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Linking, ScrollView, Switch} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {useTheme} from 'styled-components/native';
+
+// Components
+import ListItem from '../components/ListItem';
+
+// Redux
+import {setTheme} from '../actions/setting.actions';
+import {themeSelector} from '../selectors/setting.selectors';
+
+const icons = {
+  darkMode: {
+    name: 'moon-outline',
+    type: 'ionicon',
+    size: 28,
+  },
+  info: {
+    name: 'alert-circle-outline',
+    type: 'ionicon',
+    size: 28,
+  },
+  flag: {
+    name: 'flag-outline',
+    type: 'ionicon',
+    size: 28,
+  },
+  mail: {
+    name: 'mail-outline',
+    type: 'ionicon',
+    size: 28,
+  },
+};
+
+const onPressReport = () => {
+  Linking.openURL(
+    'mailto:trvathin@gmail.com?subject=Góp ý, báo cáo&body=Ý kiến của bạn: %20',
+  );
+};
 
 function SettingsScreen(props) {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+
+  const themeMode = useSelector(themeSelector);
+
   return (
-    <>
-      <View>
-        <Text>SettingsScreen</Text>
-      </View>
-    </>
+    <ScrollView style={styles.scroll}>
+      <ListItem
+        iconProps={icons.darkMode}
+        title={'Chế độ tối'}
+        subtitle={'Chuyển giao diện tối'}
+        rightElement={
+          <Switch
+            thumbColor={
+              themeMode === 'light' ? theme.elevatedBG : theme.foreground
+            }
+            value={themeMode === 'dark'}
+            trackColor={{
+              true: `${theme.fgTrans}0.3))`,
+              false: `${theme.fgTrans}0.3))`,
+            }}
+            disabled
+          />
+        }
+        onPress={() =>
+          dispatch(setTheme(themeMode === 'light' ? 'dark' : 'light'))
+        }
+      />
+
+      <ListItem
+        iconProps={icons.mail}
+        title={'Góp ý, báo lỗi'}
+        subtitle={'Gửi email cho chúng tôi'}
+        onPress={() => onPressReport()}
+      />
+
+      <ListItem iconProps={icons.info} title={'Phiên bản'} subtitle={'0.01'} />
+    </ScrollView>
   );
 }
 
 export default SettingsScreen;
+
+const styles = {
+  scroll: {
+    flex: 1,
+    marginTop: 20,
+    marginBottom: 60,
+  },
+};
