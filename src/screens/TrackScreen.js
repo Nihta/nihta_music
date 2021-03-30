b213c0125f6bc5d9a7be31aa7c46fb6e7a4a7424
Thin Tran <trvathin@gmail.com>
2021-03-30 16:57:14 +0700

    Using Track component

--- src/screens/TrackScreen.js
+++ src/screens/TrackScreen.js
@@ -1,14 +1,150 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
+import styled from 'styled-components/native';
+import Track from '../components/Track';
 
 function TracksScreen(props) {
+  const trackData = [
+    {
+      id: '000',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '001',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '002',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '003',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '004',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '005',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '006',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '007',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '008',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '009',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+
+    {
+      id: '010',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '011',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '012',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+
+    {
+      id: '013',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '014',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '015',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '016',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '017',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '018',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '019',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+    {
+      id: '020',
+      title: 'Tên bài hát',
+      artist: 'Ca sĩ',
+    },
+  ];
+
+  if (trackData === 0) {
+    return (
+      <MessageWrapper>
+        <Message numberOfLines={2}>
+          {'Không tìm thấy bất kì bản nhạc nào trên thiết bị của bạn'}
+        </Message>
+      </MessageWrapper>
+    );
+  }
+
   return (
     <>
-      <View>
-        <Text>TracksScreen</Text>
-      </View>
+      <FlatList
+        keyExtractor={asset => asset.id.toString()}
+        data={trackData}
+        renderItem={({item}) => <Track item={item} />}
+      />
     </>
   );
 }
+
+const MessageWrapper = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Message = styled.Text`
+  font-size: 16px;
+  margin: 0 55px 0 55px;
+  text-align: center;
+`;
 
 export default TracksScreen;
