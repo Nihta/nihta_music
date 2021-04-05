6adec0bce74df07049129aaa61f676ebd4f1cc40
Thin Tran <trvathin@gmail.com>
2021-04-05 16:48:53 +0700

    Test  require Permissions

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {
+  checkStoragePermissions,
+  getStoragePermission,
+} from '../utils/Permissions';
 
 function SearchScreen(props) {
+  useEffect(() => {
+    const cpdm = async () => {
+      let granted = await checkStoragePermissions();
+
+      console.log(granted);
+      if (!granted) {
+        await getStoragePermission();
+      }
+    };
+
+    try {
+      cpdm();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <View>
