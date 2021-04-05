493181a2a3911edb2c2382c6769bcb106b70e338
Thin Tran <trvathin@gmail.com>
2021-04-05 20:47:49 +0700

    Update

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {Text, View} from 'react-native';
-import {
-  checkStoragePermissions,
-  getStoragePermission,
-} from '../utils/Permissions';
 
 function SearchScreen(props) {
-  useEffect(() => {
-    const cpdm = async () => {
-      let granted = await checkStoragePermissions();
-
-      console.log(granted);
-      if (!granted) {
-        await getStoragePermission();
-      }
-    };
-
-    try {
-      cpdm();
-    } catch (error) {
-      console.log(error);
-    }
-  }, []);
-
   return (
     <>
       <View>
