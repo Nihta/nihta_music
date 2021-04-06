0189f42299dcd793fcf14dc054adba99029b65a3
Thin Tran <trvathin@gmail.com>
2021-04-06 08:29:04 +0700

    Add playbackService

---
+++ src/services/playbackService.js
@@ -0,0 +1,42 @@
+import TrackPlayer from 'react-native-track-player';
+
+// * https://react-native-track-player.js.org/getting-started/#playback-service
+// * https://react-native-track-player.js.org/documentation/#events
+module.exports = async function () {
+  /**
+   * Fired when the user presses the play button.
+   * Only fired if the CAPABILITY_PLAY is allowed.
+   */
+  TrackPlayer.addEventListener('remote-play', () => {
+    TrackPlayer.play();
+  });
+
+  /**
+   * Fired when the user presses the pause button.
+   * Only fired if the CAPABILITY_PAUSE is allowed or if there’s a change in
+   * outputs (e.g.: headphone disconnected).
+   */
+  TrackPlayer.addEventListener('remote-pause', () => {
+    TrackPlayer.pause();
+  });
+
+  /**
+   * Fired when the user presses the next track button.
+   * Only fired if the CAPABILITY_SKIP_TO_NEXT is allowed.
+   */
+  TrackPlayer.addEventListener('remote-next', () => {});
+
+  /**
+   * Fired when the user presses the previous track button.
+   * Only fired if the CAPABILITY_SKIP_TO_PREVIOUS is allowed.
+   */
+  TrackPlayer.addEventListener('remote-previous', () => {});
+
+  /**
+   * Fired when the queue reaches the end.
+   */
+  TrackPlayer.addEventListener(
+    'playback-queue-ended',
+    ({position, track}) => {},
+  );
+};
