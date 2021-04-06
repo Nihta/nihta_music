a18dec9c8da4765ffd83a58bdcf96bd3c9a61944
Thin Tran <trvathin@gmail.com>
2021-04-06 09:47:50 +0700

    Add redux-logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,18 @@
 import ReduxThunk from 'redux-thunk';
+import {createLogger} from 'redux-logger';
 import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
 import rootReducer from '../reducers';
 
+const logger = createLogger({
+  colors: false,
+});
+
 function configureStore() {
   const composeEnhancers = composeWithDevTools({});
 
-  const middlewares = [ReduxThunk];
+  const middlewares = [ReduxThunk, logger];
 
   return createStore(
     rootReducer,
