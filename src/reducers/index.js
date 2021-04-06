93d10faeed60ed4eaa982fc9a7172bc61623d94a
Thin Tran <trvathin@gmail.com>
2021-04-06 10:09:04 +0700

    Add musicPlayer (redux)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import {combineReducers} from 'redux';
 
+// Reducer
 import settingReducer from './setting.reducer';
+import musicPlayerReducer from './musicPlayer.reducer';
 
 const rootReducer = combineReducers({
   setting: settingReducer,
+  musicPlayer: musicPlayerReducer,
 });
 
 export default rootReducer;
