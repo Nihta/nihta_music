c295a5000316172fb988d4ae5a716017ef25d1bb
Thin Tran <trvathin@gmail.com>
2021-04-07 15:58:20 +0700

    Add mediaReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 
 // Reducer
 import settingReducer from './setting.reducer';
+import mediaReducer from './mediaReducer';
 import musicPlayerReducer from './musicPlayer.reducer';
 
 const rootReducer = combineReducers({
   setting: settingReducer,
+  media: mediaReducer,
   musicPlayer: musicPlayerReducer,
 });
 
---
+++ src/reducers/mediaReducer.js
@@ -0,0 +1,62 @@
+import MusicFiles from '@yajanarao/react-native-get-music-files';
+// * https://github.com/reduxjs/reselect
+// import {createSelector} from 'reselect';
+
+import handleMediaData from './utils/handleMediaData';
+
+// * Đặt tên action kiểu domain/action
+export const SET_MEDIA_FILES = 'media/SET_MEDIA_FILES';
+
+// * Actions -------------------------------------------------------------------
+const options = {
+  title: true,
+  artist: true,
+  album: true,
+  duration: true,
+  cover: true,
+  coverFolder: '.nihta_music',
+  blured: false,
+  genre: true,
+};
+
+/**
+ * Tìm files nhạc có trên máy
+ */
+export const getMedia = () => async dispatch => {
+  try {
+    const data = await MusicFiles.getAll(options);
+    console.log('Cover: ', data[0].cover);
+    console.log(data);
+
+    dispatch({
+      type: SET_MEDIA_FILES,
+      payload: handleMediaData(data),
+    });
+  } catch (reason) {
+    throw reason;
+  }
+};
+
+// * Selector ------------------------------------------------------------------
+export const mediaFilesSelector = state => state.media.mediaFiles;
+
+// * Reducer -------------------------------------------------------------------
+const initialState = {
+  mediaFiles: [],
+  artists: [],
+  albums: [],
+  mediaLoaded: false,
+};
+
+export default (state = initialState, {type, payload}) => {
+  switch (type) {
+    case SET_MEDIA_FILES:
+      return {
+        ...state,
+        mediaFiles: payload,
+      };
+
+    default:
+      return state;
+  }
+};
