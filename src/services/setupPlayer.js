1467cd41a90b88f14223f755321d0812a9db95be
Thin Tran <trvathin@gmail.com>
2021-04-07 11:23:34 +0700

    Add media controls

--- src/services/setupPlayer.js
+++ src/services/setupPlayer.js
@@ -2,10 +2,9 @@ import TrackPlayer from 'react-native-track-player';
 
 // * https://react-native-track-player.js.org/getting-started/
 export default async function setupPlayer() {
-  // Await the player is ready to be used
   await TrackPlayer.setupPlayer();
 
-  TrackPlayer.updateOptions({
+  await TrackPlayer.updateOptions({
     stopWithApp: true,
     // Media controls capabilities
     capabilities: [
@@ -13,6 +12,8 @@ export default async function setupPlayer() {
       TrackPlayer.CAPABILITY_PAUSE,
       TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
       TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      TrackPlayer.CAPABILITY_SEEK_TO,
+      TrackPlayer.CAPABILITY_STOP,
     ],
 
     // Capabilities that will show up when the notification is in the compact
@@ -20,6 +21,15 @@ export default async function setupPlayer() {
     compactCapabilities: [
       TrackPlayer.CAPABILITY_PLAY,
       TrackPlayer.CAPABILITY_PAUSE,
+    ],
+
+    notificationCapabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+      TrackPlayer.CAPABILITY_SKIP_TO_NEXT,
+      TrackPlayer.CAPABILITY_SKIP_TO_PREVIOUS,
+      TrackPlayer.CAPABILITY_SEEK_TO,
+      TrackPlayer.CAPABILITY_STOP,
     ],
 
     // Icons for the notification on Android (if you don't like the default ones)
