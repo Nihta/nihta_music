52e29e28ea4ad971615134822c1766b6e50a3ac7
Thin Tran <trvathin@gmail.com>
2021-04-11 23:34:38 +0700

    Remove getRandomNumber

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,7 @@
 export * from './permission';
 
 /**
+ * TODO: Thêm param số bỏ qua mục đích để random tránh bài hiện tại
  * Generates a random number in the specified range.
  * @param {number} min
  * @param {number} max
@@ -9,9 +10,4 @@ export * from './permission';
 export const randomNumberInRange = (min, max) => {
   const rn = Math.random() * (max - min) + min;
   return Math.trunc(rn);
-};
-
-export const getRandomNumber = (min, max) => {
-  let random = Math.round(Math.random() * (max - min) + min);
-  return random >= max ? random - 1 : random;
 };
