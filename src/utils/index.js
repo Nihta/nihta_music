86c08437e2d50b963dd3d8a962757d1144d24d7b
Thin Tran <trvathin@gmail.com>
2021-04-11 23:13:30 +0700

    Add randomNumberInRange util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,16 @@
 export * from './permission';
 
+/**
+ * Generates a random number in the specified range.
+ * @param {number} min
+ * @param {number} max
+ * @returns {number}
+ */
+export const randomNumberInRange = (min, max) => {
+  const rn = Math.random() * (max - min) + min;
+  return Math.trunc(rn);
+};
+
 export const getRandomNumber = (min, max) => {
   let random = Math.round(Math.random() * (max - min) + min);
   return random >= max ? random - 1 : random;
