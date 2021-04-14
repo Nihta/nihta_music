13d1f1f7dd4ea820d7f36e1449539f68931a6355
Thin Tran <trvathin@gmail.com>
2021-04-14 22:46:57 +0700

    Add PlayerFooter

--- src/containers/PlayerFooter.js
+++ src/containers/PlayerFooter.js
@@ -1,117 +1,33 @@
 import React from 'react';
-import {Dimensions, TouchableWithoutFeedback} from 'react-native';
-import styled, {useTheme, withTheme} from 'styled-components/native';
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import styled, {useTheme} from 'styled-components/native';
 import {useTrackPlayerProgress} from 'react-native-track-player/lib/hooks';
-import Icon from '../components/Icon';
-import ProgressBar from '../components/ProgressBar';
-import {useSelector} from 'react-redux';
-import {selectCurrentTrack, setIsPlaying} from '../reducers/musicPlayerReducer';
 
+// Navigations
 import {navigate} from '../navigations/utils/navigationServices';
 
+// Redux
+import {
+  selectCurrentTrack,
+  selectIsPlaying,
+  setIsPlaying,
+} from '../reducers/musicPlayerReducer';
+
+// Components
+import Icon from '../components/Icon';
+import ProgressBar from '../components/ProgressBar';
+import PressableIcon from '../components/PressableIcon';
+
 const placeholder = require('../../assets/images/placeholder.jpg');
-
 const SCREEN_WIDTH = Dimensions.get('window').width;
-
-function PlayerFooter(props) {
-  const theme = useTheme();
-  const currentTrack = useSelector(selectCurrentTrack);
-  const isPlaying = useSelector(setIsPlaying);
-
-  const renderFooter = isPlaying;
-
-  const {position, duration} = useTrackPlayerProgress(100);
-
-  function togglePlayback() {}
-
-  const progress = position / duration;
-  const coverSrc = currentTrack?.artwork
-    ? {uri: currentTrack.artwork}
-    : placeholder;
-  return renderFooter && currentTrack.id !== '000' ? (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        navigate('player');
-      }}>
-      <MainWrapper>
-        <Thumbnail source={coverSrc} />
-        <TextWrapper>
-          <Title numberOfLines={1}>{currentTrack.title || 'unknown'}</Title>
-          <Artist numberOfLines={1}>{currentTrack.artist || 'unknown'}</Artist>
-        </TextWrapper>
-        {isPlaying ? (
-          <StyledIcon {...icons.pauseIcon} onPress={togglePlayback} />
-        ) : (
-          <StyledIcon {...icons.playIcon} onPress={togglePlayback} />
-        )}
-        <ProgressWrapper>
-          <Progress
-            progress={isNaN(progress) ? 0 : +progress.toFixed(3)}
-            color={theme.foreground}
-          />
-        </ProgressWrapper>
-      </MainWrapper>
-    </TouchableWithoutFeedback>
-  ) : null;
-}
-
-export default PlayerFooter;
-
-const MainWrapper = styled.View`
-  height: 60px;
-  position: absolute;
-  left: 0px;
-  right: 0px;
-  bottom: 49px;
-  flex-direction: row;
-  align-items: center;
-  padding-left: 15px;
-  background-color: red;
-`;
-
-const Thumbnail = styled.Image`
-  height: 42px;
-  width: 42px;
-  border-radius: 21px;
-`;
-
-const TextWrapper = styled.View`
-  height: 75%;
-  flex: 1;
-  flex-direction: column;
-  justify-content: space-evenly;
-  margin-left: 15px;
-`;
-
-const Title = styled.Text`
-  font-family: 'CircularBold';
-  font-size: 14px;
-  color: white;
-  width: ${SCREEN_WIDTH / 2}px;
-`;
-
-const Artist = styled.Text`
-  font-family: 'CircularLight';
-  font-size: 12px;
-  color: #ccc;
-  width: ${SCREEN_WIDTH / 2}px;
-`;
-
-const StyledIcon = styled(Icon)`
-  color: white;
-  padding: 18px;
-`;
-
-const ProgressWrapper = styled.View`
-  position: absolute;
-  top: 0;
-`;
-
-const Progress = styled(ProgressBar)`
-  height: 2px;
-  width: ${SCREEN_WIDTH}px;
-  background-color: #cfc;
-`;
 
 const icons = {
   playIcon: {
@@ -125,3 +41,111 @@ const icons = {
     size: 24,
   },
 };
+
+function PlayerFooter() {
+  const theme = useTheme();
+  const dispatch = useDispatch();
+  const isPlaying = useSelector(selectIsPlaying);
+  const currentTrack = useSelector(selectCurrentTrack);
+
+  const togglePlayback = () => {
+    dispatch(setIsPlaying(!isPlaying));
+  };
+
+  const showFooter = !!currentTrack;
+  const coverSrc = currentTrack?.artwork
+    ? {uri: currentTrack.artwork}
+    : placeholder;
+  const {position, duration} = useTrackPlayerProgress(100);
+  const progress = position / duration;
+
+  return (
+    <>
+      {showFooter && (
+        <>
+          <TouchableWithoutFeedback
+            onPress={() => {
+              navigate('player');
+            }}>
+            <View style={styles.mainWrapper}>
+              <Image style={styles.thumbnail} source={coverSrc} />
+
+              <View style={styles.textWrapper}>
+                <Text style={styles.title} numberOfLines={1}>
+                  {currentTrack.title || 'unknown'}
+                </Text>
+                <Text style={styles.artist} numberOfLines={1}>
+                  {currentTrack.artist || 'unknown'}
+                </Text>
+              </View>
+
+              <PressableIcon
+                iconStyle={styles.icon}
+                iconProps={isPlaying ? icons.pauseIcon : icons.playIcon}
+                onPress={togglePlayback}
+              />
+
+              <View style={styles.progressWrapper}>
+                <ProgressBar
+                  style={styles.progress}
+                  progress={isNaN(progress) ? 0 : +progress.toFixed(3)}
+                  color={theme.primary}
+                />
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </>
+      )}
+    </>
+  );
+}
+
+export default PlayerFooter;
+
+const styles = StyleSheet.create({
+  mainWrapper: {
+    height: 60,
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 49,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: 15,
+    backgroundColor: 'white',
+  },
+  icon: {
+    color: 'white',
+  },
+  thumbnail: {
+    height: 42,
+    width: 42,
+    borderRadius: 21,
+  },
+  textWrapper: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    height: '75%',
+    marginLeft: 15,
+  },
+  progressWrapper: {
+    position: 'absolute',
+    top: 0,
+  },
+  progress: {
+    height: 2,
+    width: SCREEN_WIDTH,
+    backgroundColor: '#ccc',
+  },
+  title: {
+    fontSize: 14,
+    color: 'black',
+    width: SCREEN_WIDTH / 2,
+  },
+  artist: {
+    fontSize: 12,
+    color: 'rgba(0,0,0,0.6)',
+    width: SCREEN_WIDTH / 2,
+  },
+});
--- src/navigations/AppNavigation.js
+++ src/navigations/AppNavigation.js
@@ -11,6 +11,7 @@ import LibraryNavigator from './LibraryNavigator';
 import TracksScreen from '../screens/TrackScreen';
 import SearchScreen from '../screens/SearchScreen';
 import SettingsScreen from '../screens/SettingScreen';
+import PlayerFooter from '../containers/PlayerFooter';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -68,36 +69,40 @@ const iconProvider = route => {
 
 function AppNavigation(props) {
   return (
-    <BottomTabs.Navigator
-      initialRouteName="Tracks"
-      backBehavior="initialRoute"
-      tabBarOptions={tabBarOptions}>
-      <BottomTabs.Screen
-        name="Tracks"
-        component={TracksScreen}
-        options={{
-          tabBarIcon: iconProvider('Tracks'),
-        }}
-      />
+    <>
+      <BottomTabs.Navigator
+        initialRouteName="Tracks"
+        backBehavior="initialRoute"
+        tabBarOptions={tabBarOptions}>
+        <BottomTabs.Screen
+          name="Tracks"
+          component={TracksScreen}
+          options={{
+            tabBarIcon: iconProvider('Tracks'),
+          }}
+        />
 
-      <BottomTabs.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{tabBarIcon: iconProvider('Search')}}
-      />
+        <BottomTabs.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{tabBarIcon: iconProvider('Search')}}
+        />
 
-      <BottomTabs.Screen
-        name="Library"
-        component={LibraryNavigator}
-        options={{tabBarIcon: iconProvider('Library')}}
-      />
+        <BottomTabs.Screen
+          name="Library"
+          component={LibraryNavigator}
+          options={{tabBarIcon: iconProvider('Library')}}
+        />
 
-      <BottomTabs.Screen
-        name="Settings"
-        component={SettingsScreen}
-        options={{tabBarIcon: iconProvider('Settings')}}
-      />
-    </BottomTabs.Navigator>
+        <BottomTabs.Screen
+          name="Settings"
+          component={SettingsScreen}
+          options={{tabBarIcon: iconProvider('Settings')}}
+        />
+      </BottomTabs.Navigator>
+
+      <PlayerFooter />
+    </>
   );
 }
 
