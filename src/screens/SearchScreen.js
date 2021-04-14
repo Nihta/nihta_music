3cac7e004784846724794e0bf17f0e0460e0cf4b
Thin Tran <trvathin@gmail.com>
2021-04-14 20:27:24 +0700

    Add SafeAreaView

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-function SearchScreen(props) {
+function SearchScreen() {
   return (
-    <>
+    <SafeAreaView>
       <View>
         <Text>SearchScreen</Text>
       </View>
-    </>
+    </SafeAreaView>
   );
 }
 
