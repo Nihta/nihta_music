3451fcd7431b6d2494325b7b41177caa72ed333e
Thin Tran <trvathin@gmail.com>
2021-04-15 22:51:55 +0700

    Add getArtists

--- src/reducers/utils/handleMediaData.js
+++ src/reducers/utils/handleMediaData.js
@@ -1,9 +1,13 @@
+import _ from 'lodash/';
+
 /**
  *
  * @param {any[]} mediaData
  * @returns
  */
 const handleMediaData = mediaData => {
+  // console.log(mediaData);
+
   const newMediaData = mediaData.map((media, idx) => {
     // * https://react-native-track-player.js.org/documentation/#track-object
     return {
@@ -27,12 +31,36 @@ const handleMediaData = mediaData => {
     };
   });
 
+  getArtists(newMediaData);
+
   return newMediaData;
 };
 
 const getFolder = path => {
   const dirArr = path.split('/');
   return dirArr[dirArr.length - 2];
+};
+
+// TODO tạo một action trong reducer
+/**
+ * Lấy danh sách các nghệ sĩ và lọc ra bài hát của họ
+ * @param mediaData
+ * @returns {{artist: string, tracks: *[]}[]}
+ */
+const getArtists = mediaData => {
+  const data = _.groupBy(mediaData, 'artist');
+
+  const artists = Object.keys(data);
+
+  /**
+   * @type {{artist: string, tracks: *[]}[]}
+   */
+  const dataArtists = artists.map(artist => ({
+    artist,
+    tracks: data[artist],
+  }));
+
+  return _.sortBy(dataArtists, 'artist');
 };
 
 export default handleMediaData;
