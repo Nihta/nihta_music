c6091b215fa4afd79076fb0d18a12d4d8ed1682c
Thin Tran <trvathin@gmail.com>
2021-04-25 22:34:52 +0700

    export WINDOW_WIDTH

--- src/themes/mixins.js
+++ src/themes/mixins.js
@@ -1,6 +1,6 @@
 import {Dimensions, PixelRatio} from 'react-native';
 
-const WINDOW_WIDTH = Dimensions.get('window').width;
+export const WINDOW_WIDTH = Dimensions.get('window').width;
 
 const guidelineBaseWidth = 375;
 
