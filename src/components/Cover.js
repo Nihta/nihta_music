1446e3900aff0c7fc496aa112c7947de5e0d632a
Thin Tran <trvathin@gmail.com>
2021-04-27 17:35:00 +0700

    Update styles

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import {Dimensions, Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 
-const placeholder = require('../../assets/images/placeholder.jpg');
+import {WINDOW_WIDTH} from '../themes/mixins';
 
-const ImageSize = Dimensions.get('window').width * 0.82;
+const ImageSize = WINDOW_WIDTH * 0.8;
 
+/**
+ *
+ * @typedef {object} Props
+ * @property {string} src
+ * @property {import('react-native').StyleProp<import('react-native').TextStyle} style
+ * @param {Props} props
+ */
 function Cover(props) {
-  const imgSrc = props.src ? {uri: props.src} : placeholder;
+  const imgSrc = props.src
+    ? {uri: props.src}
+    : require('../../assets/images/placeholder.jpg');
 
-  return <Image style={styles.cover} source={imgSrc} />;
+  return <Image style={[styles.cover, props.style]} source={imgSrc} />;
 }
 
 const styles = StyleSheet.create({
