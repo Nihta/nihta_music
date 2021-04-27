37b6e1fee2041493b9308829f168646b2d3d992c
Thin Tran <trvathin@gmail.com>
2021-04-27 16:16:01 +0700

    Using TouchableIcon

--- src/screens/ArtistScreen.js
+++ src/screens/ArtistScreen.js
@@ -7,13 +7,18 @@ import {useNavigation} from '@react-navigation/core';
 import {getArtists, selectArtists} from '../reducers/mediaReducer';
 
 // Components
-import PressableIcon from '../components/PressableIcon';
 import ListItem from '../components/ListItem';
+import TouchableIcon from '../components/TouchableIcon';
 
 const icons = {
   folder: {
     name: 'folder-open-outline',
     type: 'ionicon',
+    size: 30,
+  },
+  more: {
+    type: 'ionicon',
+    name: 'ios-ellipsis-vertical',
     size: 26,
   },
 };
@@ -49,15 +54,11 @@ function ArtistsScreen(props) {
               title={item.artist}
               subtitle={`${item.tracks.length} bài hát`}
               iconProps={icons.folder}
-              rightElement={
-                <PressableIcon
-                  iconProps={{
-                    type: 'ionicon',
-                    name: 'ios-ellipsis-vertical',
-                    size: 25,
-                  }}
-                />
-              }
+              // eslint-disable-next-line react-native/no-inline-styles
+              style={{
+                paddingRight: 6,
+              }}
+              rightElement={<TouchableIcon iconProps={icons.more} />}
               onPress={() => {
                 navigation.navigate('track-list-filter', {
                   name: item.artist,
