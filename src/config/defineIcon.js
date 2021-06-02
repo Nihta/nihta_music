b67493d938491a2a2d3e164ff14b595787b56d3c
Thin Tran <trvathin@gmail.com>
2021-06-02 16:47:46 +0700

    init

---
+++ src/config/defineIcon.js
@@ -0,0 +1,24 @@
+export const trashIcon = {
+  name: 'trash',
+  type: 'feather',
+};
+
+export const discIcon = {
+  name: 'disc',
+  type: 'feather',
+};
+
+export const hiddenIcon = {
+  name: 'trash',
+  type: 'feather',
+};
+
+export const albumsIcon = {
+  name: 'albums-outline',
+  type: 'ionicon',
+};
+
+export const artistIcon = {
+  name: 'person-outline',
+  type: 'ionicon',
+};
